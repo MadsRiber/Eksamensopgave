0ac9a80beb660522cc82bb96d8e39a98bc53edf8
Mads <mads>
2020-11-06 15:00:30 +0100

    admin login opsat

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const dbName = "Eksamensprojekt"
+
 
 
 
 //Jeg kan nu lave routes
-const userRoute = require("./Views/Routes/Users")
+const userRoute = require("./Views/Routes/Users");
+const adminRoute = require("./Views/Routes/Admin");
 
 mongoose.connect(process.env.DB_CONNECTION,
-    {useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Connected to DB"))
+    {useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Connected to DB"));
     
 app.use(bodyParser.json());
 
 app.use("/users", userRoute);
+app.use("/admin", adminRoute);
 app.use(express.static("./Views"));
 
 
@@ -23,17 +25,6 @@ app.set("view-engine", "ejs");
 
 app.get("/", function(req, res) {
     res.render("index.ejs")
-})
-/*app.get("/users", function(req, res){
-    const db = client.db(dbName)
-    const collection = db.collection("users")
-    collection.find({}).toArray(function(err, users){
-    assert.equal(err, null);
-    res.render("homepage.ejs", {"users": users})
-    });
-      
-})*/
-
-
+});
 
 app.listen(3000);
